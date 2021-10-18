7c29a8a5e06e856449d9cd72dffd473b05cb9a5b
James <35617601+jliby@users.noreply.github.com>
2021-10-18 18:06:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const menu = require("./menu");
+const { autoUpdater } = require("electron-updater");
+let window;
+app.on("ready", () => {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true },
+  });
+  window.loadFile("index.html");
+  autoUpdater.checkForUpdatesAndNotify();
+});
+Menu.setApplicationMenu(menu);
+GH_TOKEN = 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;
